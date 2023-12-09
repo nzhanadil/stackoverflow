b7512372e43886f2548d55788a25ddccd07c19b8
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-09 15:44:36 -0600

    first changes

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner scanner = new Scanner(System.in);
+
+    // Day 1
+    public void findOccurrenceOfInt(){
+        System.out.println("Please enter numbers separated by space");
+        String[] strArr = scanner.nextLine().split(" ");
+
+        System.out.println("Which numbers occurrence you would like to count?");
+        int numToSearch = scanner.nextInt();
+
+        int count = 0;
+
+        for (int i = 0; i < strArr.length; i++) {
+            try {
+                if (Integer.parseInt(strArr[i]) == numToSearch) count++;
+            } catch (NumberFormatException e) {
+            }
+        }
+        System.out.println("Number " + numToSearch + "'s occurrence is: " + count);
+    }
+}
