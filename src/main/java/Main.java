32085d1c61c4482dd2c5b00b71902c5934ceb653
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-10 11:53:34 -0600

    day 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -21,5 +22,18 @@ public class Main {
             }
         }
         System.out.println("Number " + numToSearch + "'s occurrence is: " + count);
+    }
+
+    // Day 2
+    public static void stripOutFromStr(){
+        ArrayList<String> errorFullTexts = new ArrayList<>();
+        ArrayList<String> errorMessageTexts = new ArrayList<>();
+
+        //Add the text to the first spot, you can add as many as you would like
+        errorFullTexts.add("ValidationError line 23 col 40:'Bad value ius-cors for attribute name on element meta: Keyword ius-cors is not registered.'");
+
+        for(String errorFullText : errorFullTexts){
+            errorMessageTexts.add(errorFullText.substring(errorFullText.indexOf(":'")+2, errorFullText.lastIndexOf(".'")));
+        }
     }
 }
